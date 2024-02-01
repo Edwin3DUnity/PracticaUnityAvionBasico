5432dedebe04dceeea1d45129eb981a8457633e1
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-01 18:41:39 -0500

    Controlador

--- _Scripts/PlayerController.cs
+++ _Scripts/PlayerController.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.SocialPlatforms;
 
 public class PlayerController : MonoBehaviour
 {
      [SerializeField, Range(-200, 200), Tooltip("velocidad de movimiento ")]
     private float speed = 5;
+
+    [SerializeField, Range(-200,200)]private  float turnSpeed;
+    
+    private float horizontal, vertical;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -17,6 +23,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime  * speed);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed );
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontal);
     }
 }
