5bace8a514aa197560df4b4cde674846260d0ae6
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-01 17:38:05 -0500

    Se creó script movimiento de personaje y camara siguiendo al personaje

--- _Scripts/PlayerController.cs
+++ _Scripts/PlayerController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField, Range(-200, 200), Tooltip("velocidad de movimiento ")]
-    private float velMovimient = 5;
+     [SerializeField, Range(-200, 200), Tooltip("velocidad de movimiento ")]
+    private float speed = 5;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * velMovimient);
+        transform.Translate(Vector3.forward * Time.deltaTime  * speed);
     }
 }
